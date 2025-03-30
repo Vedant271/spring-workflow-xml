ef2678315aa2e9a7da84630a9130756809089b00
Vedant Karale <vedantkarale271@gmail.com>
2025-03-30 23:48:57 +0530

    singletom bean having parameter with default value

--- src/main/java/org/javaproject/springworkflowxml/App.java
+++ src/main/java/org/javaproject/springworkflowxml/App.java
@@ -12,7 +12,13 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("configuration.xml");
-        Employee e = (Employee) context.getBean("employee");
-        e.code();
+
+        // At this stage only one object is created with age having default int value
+        Employee e1 = (Employee) context.getBean("employee");
+        System.out.println(e1.age);
+//        e1.code();
+
+        Employee e2 = (Employee) context.getBean("employee");
+        System.out.println(e2.age);
     }
 }
--- src/main/java/org/javaproject/springworkflowxml/Employee.java
+++ src/main/java/org/javaproject/springworkflowxml/Employee.java
@@ -1,6 +1,12 @@
 package org.javaproject.springworkflowxml;
 
 public class Employee {
+    int age;
+
+    public Employee(){
+        System.out.println("Employee Created");
+    }
+
     public void code(){
         System.out.println("Coding");
     }
